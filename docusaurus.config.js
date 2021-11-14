6f2cfaae64b6a0b54d3e0749125a6cdd6dab23bf
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-14 01:39:17 -0700

    Add files via upload

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,58 +16,38 @@ const {
 const { Event, useEvent } = require('@agile-ts/event');
 const { toast } = require('react-toastify');
 
-const githubOrgUrl = 'https://github.com/agile-ts';
-const domain = 'https://agile-ts.org';
-const npmOrgUrl = 'https://www.npmjs.com/package/@agile-ts';
+const githubOrgUrl = 'https://github.com/ReviewBots';
+const domain = 'https://docs.reviewbots.xyz';
+const npmOrgUrl = 'https://www.npmjs.com/package/reviewbots.js';
 
 const customFields = {
-  copyright: `Made with  💜  in Germany     |    Copyright © 2020-${new Date().getFullYear()} <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/DevBenno">BennoDev</a>`,
+  copyright: `Copyright © 2020-${new Date().getFullYear()} <a target="_blank" rel="noopener noreferrer" href="https://reviewbots.xyz">Review Bots</a>`,
   meta: {
-    title: 'An atom based state manager for JavaScript apps.',
-    image: '/img/meta.png',
+    title: 'Review Bots Docs',
+    image: '/img/rb_new.png',
     description:
-      'A global State and Logic Library implemented in Typescript, ' +
-      'offering a reimagined API that focuses on developer experience. ' +
-      'AgileTs is a more straightforward alternative to Redux ' +
-      'and allows you to easily manage your application States in React, Vue and plain Javascript.',
+      'The Number One Discord Bot Review Service, Experiencing Issues with your Favourite Bots? Search our vast list of bots and Let the Dev Team know!' ,
     color: '#6c69a0',
     keywords: [
-      'state management',
-      'react',
-      'state',
-      'react state management',
-      'react native state management',
-      'react state',
-      'typescript',
-      'react state management without redux',
-      'vue',
-      'webdev',
-      'redux',
-      'recoil',
-      'mobx',
-      'javascript',
-      'software',
-      'coding',
-      'development',
-      'engineering',
+      'Discord',
+      'Bot',
+      'Reviews'
     ],
   },
   domain,
   githubOrgUrl,
-  githubUrl: `${githubOrgUrl}/agile`,
-  githubDocsUrl: `${githubOrgUrl}/documentation`,
-  npmCoreUrl: `${npmOrgUrl}/core`,
-  discordUrl: `https://discord.gg/T9GzreAwPH`,
-  stackoverflowUrl: 'https://stackoverflow.com/questions/tagged/agile-ts',
-  twitterUrl: 'https://twitter.com/AgileFramework',
-  redditUrl: 'https://www.reddit.com/r/AgileTs/',
+  githubUrl: `${githubOrgUrl}/ReviewBots-Docs`,
+  githubDocsUrl: `${githubOrgUrl}/ReviewBots-Docs`,
+  npmCoreUrl: `${npmOrgUrl}`,
+  discordUrl: `https://reviewbots.xyz/discord`,
+  twitterUrl: 'https://twitter.com/ReviewBots',
   version: '0.0.1',
   announcementBar: {
     id: 'announcement',
     content: [
-      `❓ If you have any questions, don't hesitate to join our <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/T9GzreAwPH">Community Discord</a> ️`,
-      `🎉 If you like AgileTs, give us a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/agile-ts/agile">GitHub</a>`,
-      `⏰ If you want to stay update to date, follow use on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/AgileTypescript">Twitter</a>`,
+      `❓ If you have any questions, don't hesitate to join our <a target="_blank" rel="noopener noreferrer" href="https://reviewbots.xyz/discord">Discord</a> ️`,
+      `🎉 If you like Review Bots, give us a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/ReviewBots/ReviewBots.js">GitHub</a>`,
+      `⏰ If you want to stay update to date, follow use on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ReviewBots">Twitter</a>`,
     ],
     random: false,
     interval: 100000,
@@ -91,15 +71,15 @@ const customFields = {
 };
 
 const config = {
-  title: 'AgileTs',
-  tagline: 'AgileTs is a global, flexible, spacy State and Logic Library',
+  title: 'Review Bots Docs',
+  tagline: 'The Number One Discord Bot Review Service, Experiencing Issues with your Favourite Bots? Search our vast list of bots and Let the Dev Team know!',
   url: customFields.domain,
   baseUrlIssueBanner: false,
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'AgileTs',
-  projectName: 'https://github.com/agile-ts/agile/',
+  favicon: 'img/rb_new.png',
+  organizationName: 'ReviewBots',
+  projectName: 'https://github.com/ReviewBots/ReviewBots-Docs',
   themes: ['@docusaurus/theme-live-codeblock'],
   scripts: [{ src: 'https://snack.expo.io/embed.js', async: true }], // https://github.com/expo/snack/blob/main/docs/embedding-snacks.md
   plugins: [
@@ -124,68 +104,35 @@ const config = {
       darkTheme: require('prism-react-renderer/themes/dracula'),
     },
     navbar: {
-      title: ' ',
-      hideOnScroll: true,
+      title: "Review Bots",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "RB Logo",
+        src:
+          "img/rb_new.png"
       },
       items: [
-        // left
         {
-          label: 'Get Started',
-          position: 'left',
-          items: [
-            {
-              label: 'Installation',
-              to: '/docs/installation/',
-            },
-            {
-              label: 'React',
-              to: '/docs/quick-start/react/',
-            },
-            {
-              label: 'Style Guide',
-              to: '/docs/style-guide/',
-            },
-            {
-              label: 'Examples',
-              to: '/docs/examples/',
-            },
-          ],
+          to: "docs/getting-started",
+          activeBasePath: "docs/getting-started",
+          label: "API Docs",
+          position: "left"
+        },
+        { 
+          to: "blog", 
+          label: "Blog", 
+          position: "right" 
         },
         {
-          label: 'Community',
-          position: 'left',
-          items: [
-            {
-              label: 'GitHub',
-              href: customFields.githubUrl,
-            },
-            {
-              label: 'Discord',
-              href: customFields.discordUrl,
-            },
-            {
-              label: 'Stack Overflow',
-              href: customFields.stackoverflowUrl,
-            },
-            {
-              label: 'Twitter',
-              href: customFields.twitterUrl,
-            },
-            {
-              label: 'Reddit',
-              href: customFields.redditUrl,
-            },
-          ],
+          href: "https://reviewbots.xyz/discord",
+          position: "right",
+          label: "Discord"
         },
         {
-          label: 'Documentation',
-          position: 'left',
-          to: 'docs/introduction/',
-        },
-      ],
+          href: "https://github.com/ReviewBots",
+          position: "right",
+          label: "GitHub"
+        }
+      ]
     },
     footer: {
       copyright: customFields.copyright,
@@ -196,16 +143,8 @@ const config = {
           items: [
             {
               label: 'Get Started',
-              to: 'docs/introduction',
-            },
-            {
-              label: 'Examples',
-              to: 'docs/examples',
-            },
-            {
-              label: 'React',
-              to: 'docs/quick-start/react',
-            },
+              to: 'docs/getting-started',
+            }
           ],
         },
         {
@@ -216,33 +155,12 @@ const config = {
               href: customFields.githubUrl,
             },
             {
-              label: 'Stack Overflow',
-              href: customFields.stackoverflowUrl,
-            },
-            {
               label: 'Discord',
               href: customFields.discordUrl,
             },
             {
               label: 'Twitter',
               href: customFields.twitterUrl,
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Privacy Policy',
-              to: '/legal/privacy-notice',
-            },
-            {
-              label: 'Cookie Policy',
-              to: '/legal/cookie-notice',
-            },
-            {
-              label: 'Blog',
-              to: '/blog/',
             },
           ],
         },
